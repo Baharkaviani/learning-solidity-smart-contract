8fdb2f64845bdf9879bae8cd7529932660b1f7c7
Bahar Kaviani <bhr.kaviani@gmail.com>
2022-07-26 16:48:50 +0430

    add TodoList.sol and edit trffle config

---
+++ contracts/TodoList.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract TodoList {
+    // state variables
+    uint public taskCount = 0; // task counter variable which shows the numbers of tasks in whole blockchain and smart contract
+}
