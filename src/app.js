67a4d6143c2f8b6f4381a550c73f8481a1bc09c6
Bahar Kaviani <bhr.kaviani@gmail.com>
2022-07-27 14:21:30 +0430

    list tasks from contract to the app

--- src/app.js
+++ src/app.js
@@ -76,8 +76,51 @@ App = {
     // Render Account
     $("#account").html(App.account);
 
+    // Render Tasks
+    await App.renderTasks();
+
     // Update loading state
     App.setLoading(false);
+  },
+
+  renderTasks: async () => {
+    // Load the total task count from the blockchain
+    const taskCount = await App.todoList.taskCount();
+    const $taskTemplate = $(".taskTemplate");
+
+    // Render out each task with a new task template
+    for (var i = 0; i < taskCount; i++) {
+      // Fetch the task data from the blockchain
+      const task = await App.todoList.tasks(i);
+      const taskID = task[0].toNumber();
+      const taskContent = task[1];
+      const taskCompleted = task[2];
+
+      console.log(task);
+      console.log(taskID);
+      console.log(taskContent);
+
+      // Create the html template for the task
+      const $newTaskTemplate = $taskTemplate.clone();
+
+      $newTaskTemplate.find(".content").html(taskContent);
+
+      $newTaskTemplate
+        .find("input")
+        .prop("name", taskID)
+        .prop("checked", taskCompleted);
+      //   .on("click", App.toggleCompleted);
+
+      // Put the task in the correct list
+      if (taskCompleted) {
+        $("#completedTaskList").append($newTaskTemplate);
+      } else {
+        $("#taskList").append($newTaskTemplate);
+      }
+
+      // Show the task
+      $newTaskTemplate.show();
+    }
   },
 
   setLoading: (boolean) => {
