430eae0c45494fa555e32175a00143ef16546e46
Bahar Kaviani <bhr.kaviani@gmail.com>
2022-07-27 15:46:22 +0430

    test creating new tasks with test file

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     // state variables
-    uint public taskCount = 0; // task counter variable which shows the numbers of tasks in whole blockchain and smart contract
+    uint256 public taskCount = 0; // task counter variable which shows the numbers of tasks in whole blockchain and smart contract
 
     struct Task {
-        uint id;
+        uint256 id;
         string content;
         bool completed;
     }
@@ -14,15 +14,19 @@ contract TodoList {
     // another state variable
     // 'tasks' is a variable with type of 'mapping(uint => Task)'
     // which means it returns a Task if we give the id with uint type
-    mapping(uint => Task) public tasks;
+    mapping(uint256 => Task) public tasks;
 
-    constructor () public {
+    event TaskCreated(uint256 id, string content, bool completed);
+
+    constructor() public {
         // our default task in the dapp
         createTask("learning solidity");
     }
 
     function createTask(string memory _content) public {
         tasks[taskCount] = Task(taskCount, _content, false);
-        taskCount ++;
+        taskCount++;
+
+        emit TaskCreated(taskCount, _content, false);
     }
 }
--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -26,6 +26,15 @@ contract("TodoList", (accounts) => {
     assert.equal(task.id.toNumber(), taskCount.toNumber() - 1);
     assert.equal(task.content, "learning solidity");
     assert.equal(task.completed, false);
-    
+  });
+
+  it("creates tasks", async () => {
+    const result = await this.todoList.createTask("A new task");
+    const taskCount = await this.todoList.taskCount();
+    assert.equal(taskCount, 2);
+    const event = result.logs[0].args;
+    assert.equal(event.id.toNumber(), 2);
+    assert.equal(event.content, "A new task");
+    assert.equal(event.completed, false);
   });
 });
